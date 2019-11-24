d7c1f2d1dfb1946c42a0d2af604b7211f565891d
zfg <1031557353@qq.com>
2019-11-24 17:20:03 +0800

    change large image

--- LargeImageDemo/app/src/main/java/com/zfg/largeimagedemo/LargeActivity.java
+++ LargeImageDemo/app/src/main/java/com/zfg/largeimagedemo/LargeActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +17,7 @@ import java.io.InputStream;
  */
 public class LargeActivity extends Activity {
 
-    private ImageView image_view;
+    private LargeImageView image_view;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,23 +30,7 @@ public class LargeActivity extends Activity {
         try {
             InputStream inputStream = getAssets().open("flower.jpg");
 
-            //获取图片的宽高，但不加载到内存中
-            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
-            tmpOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(inputStream, null, tmpOptions);
-            int mImageWidth = tmpOptions.outWidth;
-            int mImageHeight = tmpOptions.outHeight;
-
-            //获取BitmapRegionDecoder，并设置相关参数
-            BitmapRegionDecoder mDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
-            BitmapFactory.Options mOptions = new BitmapFactory.Options();
-            mOptions.inPreferredConfig = Bitmap.Config.RGB_565;
-
-            //显示图片
-            Rect rect = new Rect(mImageWidth / 2 - 400, mImageHeight / 2 - 400,
-                    mImageWidth / 2 + 400, mImageHeight / 2 + 400);
-            Bitmap newBitmap = mDecoder.decodeRegion(rect, mOptions);
-            image_view.setImageBitmap(newBitmap);
+            image_view.setInputStream(inputStream);
 
         } catch (IOException e) {
             e.printStackTrace();
